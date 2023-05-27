3f328c75d18ee130b66cc0479c3b235f76b83a93
xARMORx <116925504+xARMORx@users.noreply.github.com>
2023-05-28 00:47:53 +0300

    add_III_VC

---
+++ RefreshRateIII/RefreshRateIII/main.cpp
@@ -0,0 +1,16 @@
+#include "main.h"
+HDC hDCScreen = GetDC(NULL);
+
+int RwD3D8EngineSetRefreshRate(int rate) {
+	rate = GetDeviceCaps(hDCScreen, VREFRESH);
+	return RwD3D8EngineSetRefreshRateHook->call<int>(rate);
+}
+
+AsiPlugin::AsiPlugin() {
+	RwD3D8EngineSetRefreshRateHook = std::make_unique<memwrapper::memhook<RwD3D8EngineSetRefreshRatePrototype>>(0x5B9890, &RwD3D8EngineSetRefreshRate);
+	RwD3D8EngineSetRefreshRateHook->install();
+}
+
+AsiPlugin::~AsiPlugin() {
+	RwD3D8EngineSetRefreshRateHook->remove();
+}
---
+++ RefreshRateIII/RefreshRateIII/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <Windows.h>
+#include <memory>
+#include "memwrapper/memwrapper.h"
+
+using RwD3D8EngineSetRefreshRatePrototype = int(__cdecl*)(int);
+std::unique_ptr<memwrapper::memhook<RwD3D8EngineSetRefreshRatePrototype>> RwD3D8EngineSetRefreshRateHook;
+
+class AsiPlugin {
+public:
+	explicit AsiPlugin();
+	virtual ~AsiPlugin();
+} AsiPlugin;
+
+#endif // !_MAIN_H_
